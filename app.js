4fa8defa3b58585af471692f896ee5daa08fc283
TaneriX07 <tanerivince@gmail.com>
2022-01-21 23:00:56 +0800

    Select the DOM nodes

--- app.js
+++ app.js
@@ -38,3 +38,12 @@ const reviews = [
   },
 ]
 
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
